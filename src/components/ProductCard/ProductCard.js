1eac77fd01fffb41610916e2faf3754411714c04
Henrique Eidt <henriqueidt@gmail.com>
2023-10-24 01:40:14 -0300

    test3

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -2,6 +2,7 @@ export const ProductCard = ({ imageUrl, name, price }) => {
   return (
     <div>
       <img src={imageUrl} alt={name} />
+      NEW PRODUCT TEST
       <h2>{name}</h2>
       <div>{price}</div>
       <button>Add to Cart</button>
