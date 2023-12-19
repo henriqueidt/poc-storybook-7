fc3af00e357f5ea4dac49e90e1f985b715cb7fe2
Henrique EIdt <henriqueidt@gmail.com>
2023-12-19 17:48:58 -0300

    Update CheckoutButton.js

--- src/components/CheckoutButton/CheckoutButton.js
+++ src/components/CheckoutButton/CheckoutButton.js
@@ -1,3 +1,3 @@
 export const CheckoutButton = () => {
-  return <button style={{backgroundColor: "green"}}>Start checkout DEMO 4</button>;
+  return <button style={{backgroundColor: "green"}}>Start checkout DEMO 5</button>;
 };
